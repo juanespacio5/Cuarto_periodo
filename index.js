3fdb8d26ad1cc07226ddf2288a5c17bc26ba4d0d
juan esteban bermudez <juan.bermudez@cea.edu.co>
2021-10-05 16:31:04 -0500

    primera clase 3er periodo

---
+++ index.js
@@ -0,0 +1,9 @@
+function sumar () {
+    let num1= parseInt(prompt("escribe un numero: "));
+    let num2= parseInt(prompt("escribe otro numero: "));    
+    let suma = num1+num2;
+    console.log(suma);
+}
+sumar();
+sumar();
+sumar();
