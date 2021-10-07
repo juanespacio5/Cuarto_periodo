58aab45c0216fb47170674ae0a62b73a8be11fa8
juan esteban bermudez <juan.bermudez@cea.edu.co>
2021-10-07 17:17:08 -0500

    Hola

--- index.js
+++ index.js
@@ -8,15 +8,28 @@ sumar();
 sumar();
 sumar();
 
-alert("jajaja xdxdxd")
+alert("jajaja xdxdxd");
 //Diseñar una funcion que capture el nombre del usuario y despues lo salude dicho nombre capturado.
 
 /* Para resolver el ejercicio necesito capturar el nombre del usuario a traves de una función*/
 
-function saludo () {
+/* function saludo () {
     let nombre = prompt("escribe tu nombre");
     let apellido = prompt("escribe tu apellido");
     let completo = nombre +" "+apellido;
     alert("bienvenido "+completo);
 }
-saludo();
+saludo(); */
+
+function bienvenida () {
+    let cargo = prompt ("Digite el nombre de su cargo en la compañia: ");
+    let nombre = prompt ("Escribe tu nombre por favor: ");
+    let tratamiento;
+    if (cargo == "gerente" || cargo == "Gerente" || cargo == "GERENTE") {
+        tratamiento = "Doctor";
+    } else {
+        tratamiento = "Señor";
+    }
+    alert("bienvenido " + tratamiento + " " + nombre);
+}
+bienvenida();
