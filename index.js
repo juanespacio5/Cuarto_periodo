0c9132cb4b2249119e5ca84ec205dc7ddad26fbc
juan esteban bermudez <juan.bermudez@cea.edu.co>
2021-11-12 12:36:05 -0500

    insertar localstorage

--- index.js
+++ index.js
@@ -60,3 +60,17 @@ window.onload = () => {
 //sin_titulo();
 
 //DOM = Document Object Model
+/* window.onload = () => {
+    const titulo = document.getElementById('titulo');
+    console.log(titulo.innerHTML);
+} */
+/* window.onload = () => {
+    const titulo = document.getElementById('titulo');
+    titulo.innerText = "Nuevo Texto";
+    const parrafo = document.getElementById('parrafo');
+    parrafo.innerText = "Mi Nuevo Parrafo";
+    parrafo.innerHTML = "<ul><li>Elemento 1</li><li>Elemento 2</li></ul>"
+} */
+
+localStorage.setItem('hola','chao','adios','hasta luego')
+localStorage.getItem('hola')
--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-/* window.onload = () => {
-    const titulo = document.getElementById('titulo');
-    console.log(titulo.innerHTML);
-} */
-/* window.onload = () => {
-    const titulo = document.getElementById('titulo');
-    titulo.innerText = "Nuevo Texto";
-    const parrafo = document.getElementById('parrafo');
-    parrafo.innerText = "Mi Nuevo Parrafo";
-    parrafo.innerHTML = "<ul><li>Elemento 1</li><li>Elemento 2</li></ul>"
-} */
-
 //Lista De Actividades --- Activity List
-const pendientes = [];
+const pendientes = JSON.parse(localStorage.getItem('pendientes')) || [];
+
 const render = () => {
-    const listado = document.getElementById("lista-pendientesq");
+    const listado = document.getElementById("lista-pendientes");
     const plantilla = pendientes.map(p => "<li>" + p + "</li>");
     listado.innerHTML = plantilla.join("");
     const elementos = document.querySelectorAll("#lista-pendientes li");
@@ -21,18 +10,27 @@ const render = () => {
         elemento.addEventListener('click',()=>{
             elemento.parentNode.removeChild(elemento);
             pendientes.splice(i,1);
+            actualizarPendientes(pendientes);
             render();
         })
     })
 }
+
+const actualizarPendientes = () => {
+    const pendientesCadena = JSON.stringify(pendientes);
+    localStorage.setItem('pendientes',pendientesCadena);
+}
+
 window.onload = () => {
+    render();
     const formulario = document.getElementById("formulario-pendientes");
     formulario.onsubmit = (e) => {
         e.preventDefault();
         const pendiente = document.getElementById("pendiente");
         const contenidoPendiente = pendiente.value;
-        pendientes.push(contenidoPendiente);
         pendiente.value = "";
+        pendientes.push(contenidoPendiente);
+        actualizarPendientes(pendientes);
         render();
     }
 }
